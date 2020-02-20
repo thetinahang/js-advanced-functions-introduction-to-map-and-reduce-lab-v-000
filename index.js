32f3d2d6a83cf8e084ca14c2e6859cda7dfba31b
Tina Hang <mstinahang@gmail.com>
2020-02-20 03:50:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+  return sourceArray.map(num => num * -1);
+};
+
+function mapToNoChange(sourceArray) {
+  return sourceArray;
+};
+
+function mapToDouble(sourceArray) {
+  return sourceArray.map(num => num * 2);
+};
+
+function mapToSquare(sourceArray) {
+  return sourceArray.map(num => num ** 2);
+};
+
+function reduceToTotal(sourceArray, index) {
+  let value = 0;
+  if (index) {
+    value = index;
+  }
+  for (const e of sourceArray) {
+    value = value + e;
+  }
+  return value;
+};
+
+function reduceToAllTrue(sourceArray) {
+  return sourceArray.reduce(function(total, value) {
+    if (total && value) {
+      return true;
+    } else {
+      return false;
+    }})
+};
+
+function reduceToAnyTrue(sourceArray) {
+  return sourceArray.reduce(function(total, value) {
+    if (total || value) {
+      return true;
+    } else {
+      return false;
+    }
+  })
+};
